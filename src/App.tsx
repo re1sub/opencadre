8048886742c4700c80af312c82c0d80c81173d57
reisub <re1sub@proton.me>
2026-07-24 20:36:45 +0200

    feat: establish basic supabase connection test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import type { Component } from "solid-js";
+import { createResource, For } from "solid-js";
+import { supabase } from "./utils/supabase";
 
-const App: Component = () => {
+async function getTodos() {
+  const { data: todos } = await supabase.from("todos").select();
+  return todos;
+}
+
+function App() {
+  const [todos] = createResource(getTodos);
+
   return (
-    <>
+    <main>
       <h1>Hello world!!!!</h1>
-    </>
+      <ul>
+        <For each={todos()}>
+          {(todo) => (
+            <li>
+              {`${todo.content} - ${new Date(todo.created_at).toLocaleString("fr-FR")}`}
+            </li>
+          )}
+        </For>
+      </ul>
+    </main>
   );
-};
+}
 
 export default App;
