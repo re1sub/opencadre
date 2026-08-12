1c9f601db78b287bfb625c3c576515ff80624cab
reisub <re1sub@proton.me>
2026-08-12 16:23:05 +0200

    style: remove Vercetti font

--- src/styles/fonts.css.ts
+++ src/styles/fonts.css.ts
@@ -1,10 +1,5 @@
 import { globalFontFace } from "@vanilla-extract/css";
 
-// import geistRegular from "#assets/fonts/Geist/Geist-Italic-VariableFont_wght.ttf";
-// import geistItalic from "#assets/fonts/Geist/Geist-VariableFont_wght.ttf";
-// import vercettiWoff from "#assets/fonts/Vercetti/Vercetti-Regular.woff";
-// import vercettiWoff2 from "#assets/fonts/Vercetti/Vercetti-Regular.woff2";
-
 export const geistFont = globalFontFace("Geist", [
 	{
 		src: `url("../assets/fonts/Geist/Geist-VariableFont_wght.ttf") format("truetype")`,
@@ -17,13 +12,3 @@ export const geistFont = globalFontFace("Geist", [
 		fontStyle: "italic",
 	},
 ]);
-
-export const vercettiFont = globalFontFace("Vercetti", {
-	src: `
-    url("../assets/fonts/Vercetti/Vercetti-Regular.woff2") format("woff2"),
-    url("../assets/fonts/Vercetti/Vercetti-Regular.woff") format("woff")
-  `,
-	fontWeight: "400",
-	fontStyle: "normal",
-	fontDisplay: "swap",
-});
