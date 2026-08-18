9821e31722da6ee8d2785fa02c37529dc6e69ad2
reisub <re1sub@proton.me>
2026-08-18 11:57:32 +0200

    feat: dynamic page title in dev

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,8 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 	throw new Error(
 		"Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
 	);
+} else if (import.meta.env.DEV) {
+	document.title += " - local";
 }
 
 render(
